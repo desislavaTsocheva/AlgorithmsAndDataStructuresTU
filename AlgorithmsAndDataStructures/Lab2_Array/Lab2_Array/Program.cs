6a2f014365e92e5985789783c299b98b4a8c14bb
desislavaTsocheva <desislava_tsocheva@abv.bg>
2025-10-23 10:52:28 +0300

    Lab 2- 23.10.25

---
+++ AlgorithmsAndDataStructures/Lab2_Array/Lab2_Array/Program.cs
@@ -0,0 +1,45 @@
+﻿int[] arr=new int[] {2,4,7,8,4,7,3,1};
+
+int min = arr[0];
+int max = arr[0];
+int counter = 0;
+
+for(int i=0; i<arr.Length; i++)
+{
+    if (min > arr[i])
+    {
+        min = arr[i];
+    }
+    if (max < arr[i])
+    {
+        max = arr[i];
+    }
+}
+
+for(int i=0;i<arr.Length; i++)
+{
+    if (arr[i] % 2 != 0 && arr[i] != min & arr[i] != max)
+    {
+        counter++;
+    }
+}
+
+int[] arr2 = new int[counter + 2];
+arr2[0] = min;
+int index = 1;
+for(int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] % 2 != 0 && arr[i] != min & arr[i]!=max)
+    {
+        
+        arr2[index] = arr[i];
+        index++;
+    }
+    
+}
+arr2[index] = max;
+
+for(int i = 0; i < arr2.Length; i++)
+{
+    Console.Write(arr2[i]+" ");
+}
---
+++ AlgorithmsAndDataStructures/Lab2_Array/Lab2_Matrix/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.Diagnostics.Metrics;
+
+Console.WriteLine("Enter row= ");
+int row = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter col= ");
+int col = int.Parse(Console.ReadLine());
+
+int[,] matrix = new int[row, col];
+for (int i = 0; i < row; i++)
+{
+    for (int j = 0; j < col; j++)
+    {
+        matrix[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+
+//MAIN DIAGONAL
+int mainDiagonal = row;
+int[] main=new int[mainDiagonal];
+for(int i = 0; i < mainDiagonal; i++)
+{
+    main[i]=matrix[i,i];
+}
+
+//SECOND DIAGONAL
+int secDiagonal = row;
+int[] sec=new int[secDiagonal];
+for (int g = 0; g < secDiagonal; g++)
+{
+    sec[g] = matrix[g, col - 1 - g];
+}
+
+//COUNTER i%2!=0;
+int counter = 0;
+for (int i = 0; i < row; i++)
+{
+    if (i % 2 != 0)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (matrix[i, j] < 0)
+            {
+                counter++;
+            }
+        }
+    }
+}
+
+//ARRAY LENGTH
+int[] array = new int[mainDiagonal + secDiagonal+counter];
+
+//ADD MAIN DIAGONAL ELEMENTS
+int index = 0;
+
+for (int i = 0; i < mainDiagonal; i++)
+{
+    array[index] = main[i];
+    index++;
+}
+
+//ADD SECOND DIAGONAL ELEMENTS
+for (int i = 0; i < secDiagonal; i++)
+{
+    array[index] = sec[i];
+    index++;
+}
+
+//ADD i%2!=0 ELEMENTS
+for (int i = 0; i < row; i++)
+{
+    if (i % 2 != 0)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (matrix[i, j] < 0)
+            {
+                array[index] = matrix[i, j];
+                index++;
+            }
+        }
+    }
+}
+
+//PRINT NEW ARRAY
+Console.WriteLine("\nResult array:");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
