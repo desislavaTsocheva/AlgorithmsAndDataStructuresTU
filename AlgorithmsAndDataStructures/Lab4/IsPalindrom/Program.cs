ef0b53e801ebb60516188039ead6c265d45c1718
desislavaTsocheva <desislava_tsocheva@abv.bg>
2025-11-20 10:42:12 +0200

    Sorting Algorithms

---
+++ AlgorithmsAndDataStructures/Lab4/IsPalindrom/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter string: ");
+        string text = Console.ReadLine();
+
+        if (IsPalindrome(text))
+            Console.WriteLine("The string IS a palindrome.");
+        else
+            Console.WriteLine("The string is NOT a palindrome.");
+    }
+
+    static bool IsPalindrome(string str)
+    {
+        int i = 0;
+        int j = str.Length - 1;
+
+        while (i < j)
+        {
+            if (str[i] != str[j])
+                return false;
+
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
---
+++ AlgorithmsAndDataStructures/Lab4/Lab4/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter sum: ");
+        int sum = int.Parse(Console.ReadLine());
+
+        Console.Write("How much money do you have (count): ");
+        int count = int.Parse(Console.ReadLine());
+
+        int[] money = new int[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write("Enter coin: ");
+            money[i] = int.Parse(Console.ReadLine());
+        }
+
+        SortDescending(money);
+
+        Console.WriteLine("\nCoins used:");
+        MakeChange(money, sum);
+    }
+
+    // Bubble Sort
+    static void SortDescending(int[] arr)
+    {
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            for (int j = 0; j < arr.Length - 1 - i; j++)
+            {
+                if (arr[j] < arr[j + 1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    static int FindGreatestCoin(int[] money, int sum)
+    {
+        for (int i = 0; i < money.Length; i++)
+        {
+            if (money[i] <= sum)
+            {
+                return money[i];
+            }
+        }
+
+        return -1; 
+    }
+
+    static void MakeChange(int[] money, int sum)
+    {
+        if (sum == 0)
+            return;
+
+        int coin = FindGreatestCoin(money, sum);
+
+        if (coin == -1)
+        {
+            Console.WriteLine("No coin fits for the remaining sum: " + sum);
+            return;
+        }
+
+        Console.WriteLine(coin);
+        MakeChange(money, sum - coin);
+    }
+}
