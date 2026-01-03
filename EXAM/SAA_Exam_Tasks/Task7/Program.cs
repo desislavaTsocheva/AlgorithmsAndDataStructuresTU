420551ef7296897bb0d140384dc4beb39e2a112d
desislavaTsocheva <desislava_tsocheva@abv.bg>
2026-01-03 23:37:44 +0200

    Completed 7&8 tasks for the exam

---
+++ EXAM/SAA_Exam_Tasks/Task7/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace Task7;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int m = int.Parse(Console.ReadLine());
+        int[,] d = new int[m, m];
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                do
+                {
+                    d[i, j] = int.Parse(Console.ReadLine());
+                } while (d[i, j] < -500 || d[i, j] > 500);
+            }
+        }
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                Console.Write(d[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        int[] minEl=new int[m];
+        int[] maxEl=new int[m];
+        for(int i = 0;i < m; i++)
+        {
+            int min = d[i, 0];
+            int max= d[i, 0];
+            for (int j = 0; j < m; j++)
+            {
+                if (d[i, j] < min) { min = d[i, j]; }
+                if (d[i, j] > max) { max = d[i, j]; }
+            }
+            minEl[i] = min;
+            maxEl[i] = max;
+        }
+
+        for(int i = 0; i < m; i++)
+        {
+            Console.Write(minEl[i]+" ");
+        }
+        Console.WriteLine();
+
+        for(int i = 0;i<m; i++)
+        {
+            Console.Write(maxEl[i]+" ");
+        }
+        Console.WriteLine();
+
+        for(int i = 0; i < m; i++)
+        {
+            d[i, 0] = minEl[i];
+            d[i,m-1]=maxEl[i];
+        }
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                Console.Write(d[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("M: ");
+        int mm= int.Parse(Console.ReadLine());
+        Console.WriteLine("N: ");
+        int n = int.Parse(Console.ReadLine());
+        int counterGreaterThanM = 0;
+        int counterSmallerThanN = 0;
+
+        for (int i = 0; i < m; i++)
+        {
+            if (maxEl[i] > mm) { counterGreaterThanM++; }
+            if (minEl[i] < n) {  counterSmallerThanN++; }
+        }
+        Console.WriteLine("Counter > m: "+counterGreaterThanM);
+        Console.WriteLine("Counter < n: "+counterSmallerThanN);
+    }
+}
---
+++ EXAM/SAA_Exam_Tasks/Task8/Program.cs
@@ -0,0 +1,75 @@
+﻿int m = int.Parse(Console.ReadLine());
+int[,] d = new int[m, m];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        do
+        {
+            d[i, j] = int.Parse(Console.ReadLine());
+        } while (d[i, j] < -999 || d[i, j] > 999);
+    }
+}
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        Console.Write(d[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] counterRow = new int[m];
+int[] counterColl = new int[m];
+for(int i = 0; i < m; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        if (d[i, j] < 0) { counterRow[i]++; }
+    }
+}
+
+for(int j = 0; j < m; j++)
+{
+    for (int i = 0; i < m; ++i)
+    {
+        if (d[i, j] < 0) { counterColl[j]++; }
+    }
+}
+
+for(int i = 0;i < m; i++)
+{
+    Console.WriteLine("Row neg: " + counterRow[i]);
+}
+
+for(int j = 0;j < m; j++)
+{
+    Console.WriteLine("Coll neg: " + counterColl[j]);
+}
+
+int min_row = counterRow[0];
+int min_row_index = 0;
+for (int i = 1; i < m; i++)
+{
+    if (counterRow[i] < min_row)
+    {
+        min_row = counterRow[i];
+        min_row_index = i;
+    }
+}
+
+int min_column = counterColl[0];
+int min_column_index = 0;
+for (int j = 1; j < m; j++)
+{
+    if (counterColl[j] < min_column)
+    {
+        min_column = counterColl[j];
+        min_column_index = j;
+    }
+}
+
+Console.WriteLine("min row: "+min_row_index);
+Console.WriteLine("min coll: "+min_column_index);
