ee86136884a7ffa63ca6107c12a6683ada3f8dbd
desislavaTsocheva <desislava_tsocheva@abv.bg>
2026-01-17 13:43:09 +0200

    Working with graph- Djikstra algorithm

---
+++ EXAM/SAA_Exam_Tasks/DjikstraAlgorithm/Program.cs
@@ -0,0 +1,61 @@
+﻿
+public class Program
+{   
+    static void Main()
+    {
+        int n = 5;
+
+        int[,] graph = {
+            { 0, 10, 0, 0, 5 },
+            { 10, 0, 1, 0, 2 },
+            { 0, 1, 0, 4, 0 },
+            { 0, 0, 4, 0, 3 },
+            { 5, 2, 0, 3, 0 }
+        };
+
+        Djikstra(n, 0, graph); 
+    }
+
+    public static void Djikstra(int n, int start, int[,] graph)
+    {
+        int[] min = new int[n];
+        bool[]visited=new bool[n];
+
+        int INF = int.MaxValue; 
+
+        for (int i = 0; i < n; i++) min[i] = INF;
+
+        min[start] = 0;
+        for(int i = 0; i < n - 1; i++)
+        {
+            int u = -1;
+            for(int j = 0; j < n; j++)
+            {
+                if (!visited[j] && (u == -1 || min[j] < min[u]))
+                {
+                    u = j;
+                }
+            }
+            visited[u] = true;
+
+            for(int j = 0; j < n; ++j)
+            {
+                if (graph[u, j] != 0 && !visited[j])
+                {
+                    int newMin = min[u]+graph[u,j];
+                    if (newMin < min[j])
+                    {
+                        min[j] = newMin;
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine("Разстояния от възел " + start + ":");
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("До възел " + i + " -> " + min[i]);
+        }
+
+    }
+}
---
+++ EXAM/SAA_Exam_Tasks/Matrix/Program.cs
@@ -0,0 +1,32 @@
+﻿int[,] matrix = {
+            { 12, 24, 36, 48 },
+            { 60, 15, 25, 72 },
+            { 84, 35, 45, 96 },
+            { 10, 20, 30, 40 }
+        };
+
+int[] arr = new int [25];
+int index = 0;
+for(int i = 0; i < matrix.GetLength(0); i++)
+{
+    arr[index]=matrix[0,i];
+    index++;
+}
+
+for(int i = 1; i < matrix.GetLength(0); i++)
+{
+    arr [index]=matrix[i,3];
+    index++;
+}
+
+for(int i=2; i<=0; i++)
+{
+    arr [index]=matrix[i,3];
+    index++;
+}
+
+for(int i = 0; i < arr.Length; i++)
+{
+    Console.Write(arr[i]+" ");
+}
+Console.WriteLine();
